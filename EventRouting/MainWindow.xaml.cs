822b8c8cf4d797cf026b15ce4bb2418a859a98a7
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-12-06 10:23:51 +0100

    PropertyBinding

--- EventRouting/MainWindow.xaml.cs
+++ EventRouting/MainWindow.xaml.cs
@@ -34,5 +34,20 @@ namespace EventRouting
         {
             Tbl_Output.Text += $"{(sender as FrameworkElement).Name}: ButtonBase.Click\n";
         }
+
+        private void SP_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if ((sender as FrameworkElement).Name == "Aqua")
+                Tbl_Output.Text += $"Origin: {(e.OriginalSource as FrameworkElement).Name}\n";
+            Tbl_Output.Text += $"{(sender as FrameworkElement).Name}: Tunneling/Preview\n";
+        }
+
+        private void SP_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Tbl_Output.Text += $"{(sender as FrameworkElement).Name}: Bubbeling\n";
+
+            if ((sender as FrameworkElement).Name == "Grün")
+                e.Handled = true;
+        }
     }
 }
