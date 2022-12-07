06805f5cc4566b091aee7244621f76408746928d
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-12-07 15:18:38 +0100

    Trigger_Lab + Korrekturen

--- Trigger/MainWindow.xaml.cs
+++ Trigger/MainWindow.xaml.cs
@@ -21,20 +21,26 @@ namespace Trigger
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool BoolVal { get;set; }
+
+
         public MainWindow()
         {
             InitializeComponent();
 
+            this.BoolVal = true;
+
+            //Setzen des DataContext
             this.DataContext = this;
         }
 
-        public bool BoolVal { get; set; } = true;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        //EventHandler zum Ändern der Property
+        private void Btn_Ändern_Click(object sender, RoutedEventArgs e)
         {
             BoolVal = !BoolVal;
+
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoolVal)));
         }
     }
