8bdbda19880cf30d867bf3d308bc302687ba8fe8
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-12-08 12:24:07 +0100

    MVVM

---
+++ Commands/CustomCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Commands
+{
+    public class CustomCommand : ICommand
+    {
+        public Action<object> ExecuteMethode { get; set; }
+        public Func<object, bool> CanExecuteMethode { get; set; }
+
+        public CustomCommand(Action<object> exe, Func<object, bool> can)
+        {
+            ExecuteMethode= exe;
+            CanExecuteMethode= can;
+        }
+
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter) => CanExecuteMethode(parameter);
+
+        public void Execute(object? parameter) => ExecuteMethode(parameter);
+    }
+}
